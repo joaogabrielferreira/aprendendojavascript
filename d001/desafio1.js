84841b8dd7e12ff17e06adb5a01ef159743c1822
joao <joao.gabrielferreira1@outlook.com>
2024-09-05 17:27:47 -0300

    Aprendendo mais sobre JavaScript

---
+++ d001/desafio1.js
@@ -0,0 +1,11 @@
+function Botao1() {
+    alert("Você clicou no 1° botão!");
+}
+
+function Botao2() {
+    alert("Você clicou no 2° botão!");
+}
+
+function Botao3() {
+    alert("Você clicou no 3° botão!");
+}
---
+++ d002/desafio2.js
@@ -0,0 +1,7 @@
+function Botao() {
+    var nome, idade;
+
+    nome = prompt("Qual é o seu nome?: ");
+    idade = Number(prompt("Olá " + nome + "! Quantos anos você tem?"));
+    alert("Acabei de conhecer " + nome + ", que tem " + idade + " anos de idade!");
+}
